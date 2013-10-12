8c3a024c51e85a19c50c31af70c28c366872438b
Hoe Kit Chew <hoekit@symhost.(none)>
2013-10-12 19:53:50 +0700

    Removed bugs and added steps to reverse actions.

--- setup_gitserver_on_EC2.sh
+++ setup_gitserver_on_EC2.sh
@@ -4,17 +4,21 @@
 # Install git if it does not exist
 which git || sudo apt-get install -y git-core 
 
-# Create user "git"
+# Create user "git" and .ssh subdirectory and empty ssh authorized_keys file
 sudo adduser git
-su git
-mkdir ~/.ssh
+sudo -u git mkdir -p ~git/.ssh
+sudo -u git touch ~git/.ssh/authorized_keys
 
 # Create git root folder
-sudo mkdir /opt/git
+sudo mkdir -p /opt/git
 sudo chown git /opt/git
 
 # Initialize project's git repository
 # mkdir /opt/git/project.git
 # cd /opt/git/project.git
 # git init --bare
+
+# Reverse steps above by removing
+# sudo rm -r /opt/git
+# sudo deluser --remove-all-files git
 
